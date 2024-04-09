f54c5f08c1f273011aa9f0f282601935e93c7767
Devansh <devanshksg@gmail.com>
2024-04-09 22:59:58 +0530

    Initial commit

---
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/Sampledelcompaose.kt
@@ -0,0 +1,12 @@
+package com.plcoding.stockmarketapp.presentation.company_listings
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun Sampledelcompaose() {
+    Text(text = "Hello, World!")
+}
